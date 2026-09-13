ddb3c1ba6aa58411d002f80f49827f5eb2f8748a
Arif Reogir <165476513+ArifReogir@users.noreply.github.com>
2026-09-13 14:24:48 +1000

    Add method documentation for parkPlane
    Added documentation comment for parkPlane method.

--- AirwayGate.java
+++ AirwayGate.java
@@ -16,6 +16,10 @@ public class AirwayGate implements drawable {
         this.currentPlane = null;
     }
 
+    /*
+ * Parks an aircraft at this gate if the gate is open and currently free.
+ * Throws OccupancyException if the gate is closed or already occupied.
+ */
     public void parkPlane(Aircraft plane) throws OccupancyException {
 
         if (!status) {
